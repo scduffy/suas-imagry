fc9950d78bc413c0d286e1ce32d5320a809abe29
Chase Duffy <scduffy@loyola.edu>
2019-03-31 14:12:52 -0400

    color detection

--- detect_color.py
+++ detect_color.py
@@ -18,7 +18,6 @@ boundaries = [
 	([25, 146, 190], [62, 174, 250]),
 	([103, 86, 65], [145, 133, 128])
 ]
-
 # loop over the boundaries
 for (lower, upper) in boundaries:
 	# create NumPy arrays from the boundaries
